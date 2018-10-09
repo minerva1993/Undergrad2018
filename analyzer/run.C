999671e90c68fa63434def68f4a18c7687fe278a
Jiwon Park <minerva1993@gmail.com>
2018-10-09 16:33:36 +0900

    run macro

---
+++ analyzer/run.C
@@ -0,0 +1,13 @@
+#include "tauAnalyzer.h"
+R__LOAD_LIBRARY(tauAnalyzer.C+)
+
+void run( const char* name )
+{
+   gSystem->CompileMacro("tauAnalyzer.C");
+
+   TChain chain("tree");
+   chain.Add(name);
+   tauAnalyzer t(&chain);
+   t.Loop();
+   gSystem->Exit(0);
+}
